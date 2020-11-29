45675e8faba045dccb46a0c2f52eb9e9d72240b2
Stefano <stefanolagattolla.s@gmail.com>
2020-11-29 13:38:28 +0100

    create incorrect check test

--- src/test/java/usantatecla/IntersectionCheckerTest.java
+++ src/test/java/usantatecla/IntersectionCheckerTest.java
@@ -2,6 +2,7 @@ package usantatecla;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class IntersectionCheckerTest {
@@ -25,5 +26,26 @@ public class IntersectionCheckerTest {
         boolean validated = checker.isThereIntersection(interval, intervalCompare);
         assertTrue(validated);
     }
+
+    @Test
+    public void testWhenThereIsIntersectionOnClosedCompareIntervalAndIncorrect(){
+        //Comes the test
+        IntersectionChecker checker = new IntersectionChecker();
+        Interval interval = new IntervalBuilder().open(2.5).open(3.3).build();
+        Interval intervalCompare = new IntervalBuilder().open(1.2).closed(2.4).build();
+        boolean validated = checker.isThereIntersection(interval, intervalCompare);
+        assertFalse(validated);
+
+    }
+    @Test
+    public void testWhenThereIsIntersectionOnOpenCompareIntervalAndIncorrect(){
+        //Comes the test
+        IntersectionChecker checker = new IntersectionChecker();
+        Interval interval = new IntervalBuilder().open(2.5).open(3.3).build();
+        Interval intervalCompare = new IntervalBuilder().open(1.2).open(2.5).build();
+        boolean validated = checker.isThereIntersection(interval, intervalCompare);
+        assertFalse(validated);
+    }
+    
 
 }
