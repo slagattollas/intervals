a4d63c6cd326fafd366f87d03fd3cf153ba81852
Stefano <stefanolagattolla.s@gmail.com>
2020-11-29 14:10:44 +0100

    create Tests when get intersections incorrect

--- src/test/java/usantatecla/IntersectionCheckerTest.java
+++ src/test/java/usantatecla/IntersectionCheckerTest.java
@@ -1,9 +1,7 @@
 package usantatecla;
 
-import org.hamcrest.Matcher;
 import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class IntersectionCheckerTest {
@@ -51,6 +49,33 @@ public class IntersectionCheckerTest {
         Interval validated = checker.getIntersection(interval, intervalCompare);
         Interval correctInterval = new IntervalBuilder().open(2.5).open(2.6).build();
         assertEquals(validated, correctInterval);
+    }
+    @Test
+    public void testGetIntersectionsValuesWhenClosedCompareIntervalCorrect(){
+        IntersectionChecker checker = new IntersectionChecker();
+        Interval interval = new IntervalBuilder().open(2.5).open(3.3).build();
+        Interval intervalCompare = new IntervalBuilder().open(1.2).closed(2.5).build();
+        Interval validated = checker.getIntersection(interval, intervalCompare);
+        Interval correctInterval = new IntervalBuilder().open(2.5).closed(2.5).build();
+        assertEquals(validated, correctInterval);
+    }
+    @Test
+    public void testGetIntersectionsValuesWhenOpenCompareIntervalInCorrect(){
+        IntersectionChecker checker = new IntersectionChecker();
+        Interval interval = new IntervalBuilder().open(2.5).open(3.3).build();
+        Interval intervalCompare = new IntervalBuilder().open(1.2).open(2.6).build();
+        Interval validated = checker.getIntersection(interval, intervalCompare);
+        Interval correctInterval = new IntervalBuilder().open(2.5).open(2.5).build();
+        assertNotEquals(validated, correctInterval);
+    }
+    @Test
+    public void testGetIntersectionsValuesWhenClosedCompareIntervalInCorrect(){
+        IntersectionChecker checker = new IntersectionChecker();
+        Interval interval = new IntervalBuilder().open(2.5).open(3.3).build();
+        Interval intervalCompare = new IntervalBuilder().open(1.2).closed(2.5).build();
+        Interval validated = checker.getIntersection(interval, intervalCompare);
+        Interval correctInterval = new IntervalBuilder().open(2.5).closed(2.6).build();
+        assertNotEquals(validated, correctInterval);
     }
 
 
