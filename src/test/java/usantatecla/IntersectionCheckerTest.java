de66e93b1bf07712270a64c62b761dbf7e21f070
Stefano <stefanolagattolla.s@gmail.com>
2020-11-29 13:36:45 +0100

    cambiar nombre de test a open interval y closed interval

--- src/test/java/usantatecla/IntersectionCheckerTest.java
+++ src/test/java/usantatecla/IntersectionCheckerTest.java
@@ -7,11 +7,21 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class IntersectionCheckerTest {
 
     @Test
-    public void testWhenThereIsIntersectionAndCorrect(){
+    public void testWhenThereIsIntersectionOnClosedCompareIntervalAndCorrect(){
         //Comes the test
         IntersectionChecker checker = new IntersectionChecker();
         Interval interval = new IntervalBuilder().open(2.5).open(3.3).build();
         Interval intervalCompare = new IntervalBuilder().open(1.2).closed(2.5).build();
+        boolean validated = checker.isThereIntersection(interval, intervalCompare);
+        assertTrue(validated);
+
+    }
+    @Test
+    public void testWhenThereIsIntersectionOnOpenCompareIntervalAndCorrect(){
+        //Comes the test
+        IntersectionChecker checker = new IntersectionChecker();
+        Interval interval = new IntervalBuilder().open(2.5).open(3.3).build();
+        Interval intervalCompare = new IntervalBuilder().open(1.2).open(2.6).build();
         boolean validated = checker.isThereIntersection(interval, intervalCompare);
         assertTrue(validated);
     }
