5767deda2292361074fd5501049464d38f8c4023
Stefano <stefanolagattolla.s@gmail.com>
2020-11-29 12:19:06 +0100

    Crear el test de intersection antes de la clase

---
+++ src/test/java/usantatecla/IntersectionTest.java
@@ -0,0 +1,18 @@
+package usantatecla;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class IntersectionTest {
+    private Intersection intersection;
+
+    @BeforeEach
+    void setup(){
+        intersection = new Intersection();
+    }
+
+    @Test
+    public testWhenThereIsIntersectionAndCorrect(){
+        //Comes the test
+    }
+}
